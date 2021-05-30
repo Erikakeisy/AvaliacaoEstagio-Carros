802faa961871db77d93fc1f1e0de2aa3a4de4e80
Erika Keisy <erika.santos_BOLS@compasso.com.br>
2021-05-30 15:15:17 -0300

    Swagger 2

--- projeto/src/main/java/com/compassouol/projeto/controller/TopicoController.java
+++ projeto/src/main/java/com/compassouol/projeto/controller/TopicoController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import com.compassouol.projeto.model.Topicos;
 import com.compassouol.projeto.repository.TopicoRepository;
+
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,15 +25,18 @@ public class TopicoController {
 	TopicoRepository TR;
 
 	@GetMapping
+	@ApiOperation("Metodo que lista todos os carros")
 	public List<Topicos> Listar() {
 		return TR.findAll();
 	}
     @PostMapping
+    @ApiOperation("Metodo que adiciona carros")
 	public Topicos cadastrar(@RequestBody Topicos Top) {
 		return TR.save(Top);
 	}
     
     @GetMapping(params="nome")
+    @ApiOperation("Metodo que busca por nome")
     public ResponseEntity<List<Topicos>> getWithFilternome(@RequestParam String nome)
     {
         return new ResponseEntity<List<Topicos>>(TR.findTopicosBynome(nome), HttpStatus.OK);
@@ -38,23 +44,27 @@ public class TopicoController {
 
 
     @GetMapping(params="marca")
+    @ApiOperation("Metodo que busca por marca")
     public ResponseEntity<List<Topicos>> getWithFilterMarca(@RequestParam String marca)
     {
         return new ResponseEntity<List<Topicos>>(TR.findTopicosBymarca(marca), HttpStatus.OK);
     }
 
     @GetMapping(params="cor")
+    @ApiOperation("Metodo que busca por cor")
     public ResponseEntity<List<Topicos>> getWithFilterCor(@RequestParam String cor)
     {
         return new ResponseEntity<List<Topicos>>(TR.findTopicosBycor(cor), HttpStatus.OK);
     }
 
     @GetMapping(params="maiscaro")
+    @ApiOperation("Metodo que busca o mais caro")
     public List<Topicos> Caro(){
         //List<Topicos> //Caro (){ //TR.Caro();
         return TR.Caro();
     }
     @GetMapping(params="maisbarato")
+    @ApiOperation("Metodo que busca o mais barato")
     public List<Topicos> Barato (){
         //List<Topicos> //Barato = TR.Barato();
         return TR.Barato();
